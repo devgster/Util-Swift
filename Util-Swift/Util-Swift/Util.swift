29bdc436b4d0912e3878455af339a255a3bf2676
Kyungwoo Park <devgster@gmail.com>
2018-02-13 09:27:26 +0900

    프로젝트 생성

---
+++ Util-Swift/Util-Swift/Util.swift
@@ -0,0 +1,18 @@
+//
+//  Util.swift
+//  Util-Swift
+//
+//  Created by Kyungwoo Park on 2018. 2. 13..
+//  Copyright © 2018년 Kyungwoo Park. All rights reserved.
+//
+
+import Foundation
+
+
+public class Util {
+    
+    public static func doSomething() -> String{
+        return "Util Swift"
+    }
+    
+}
---
+++ Util-Swift/Util-Swift/Util_Swift.h
@@ -0,0 +1,19 @@
+//
+//  Util_Swift.h
+//  Util-Swift
+//
+//  Created by Kyungwoo Park on 2018. 2. 13..
+//  Copyright © 2018년 Kyungwoo Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Util_Swift.
+FOUNDATION_EXPORT double Util_SwiftVersionNumber;
+
+//! Project version string for Util_Swift.
+FOUNDATION_EXPORT const unsigned char Util_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Util_Swift/PublicHeader.h>
+
+
